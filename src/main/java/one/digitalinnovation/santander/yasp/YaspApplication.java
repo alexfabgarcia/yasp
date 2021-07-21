646a3e5a7044e4e5a7b3f637cb4f0d67c6933002
Alex Garcia <alexfabgarcia@gmail.com>
2021-07-20 21:46:06 -0300

    First commit

---
+++ src/main/java/one/digitalinnovation/santander/yasp/YaspApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.santander.yasp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/santander/yasp/YaspApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.santander.yasp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
